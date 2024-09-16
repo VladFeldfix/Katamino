70dc74037d7130d4b6577413fdb96e3a1ac8b603
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2024-09-16 05:57:27 +0300

    general sync

--- Katamino.py
+++ Katamino.py
@@ -1,62 +1,67 @@
+from Blocks import Blocks
 class block:
     def __init__(self, size):
         self.size = size
 
 class main:
     def __init__(self):
-        self.obj_red = block(((0,1),(1,0),(1,1),(1,2),(2,1))) # (row, col)
-        #self.obj_ = block(((),(),(),(),()))
-
+        self.blocks = Blocks()
         self.board = []
+        self.playing_blocks = []
 
         ################  for testing  ################
-        self.set_board_size(5)
+        self.set_board_size(3)
         self.display_board()
-        self.add_object_to_game(self.obj_red)
+        self.add_object_to_game(self.blocks.Orange)
+        self.add_object_to_game(self.blocks.Brown)
+        self.add_object_to_game(self.blocks.Green)
         self.display_board()
+        self.place_block(0,0,self.playing_blocks[0],3)
         ###############################################
     
     def set_board_size(self, size):
         for row in range(5):
             self.board.append([])
-            for col in range(size):
+            for _ in range(size):
                 self.board[row].append(0)
 
     def add_object_to_game(self, obj_block):
+        self.playing_blocks.append(obj_block)
         y = 0
         x = 0
         for row in self.board:
-            for col in row:
+            for _ in row:
                 self.create_ghost(y,x,obj_block)
                 x += 1
             x = 0
             y += 1
 
-    def create_ghost(self, x, y, obj_block):
-        shape = []
-        fail = False
-        for cell in obj_block.size:
-            xoffset = cell[0]
-            yoffset = cell[1]
-            try:
-                test = self.board[x+xoffset][y+yoffset]
-                shape.append((x+xoffset,y+yoffset))
-            except:
-                shape = []
-                fail = True
-        if not fail:
-            for add in shape:
-                self.board[add[0]][add[1]] += 1
+    def create_ghost(self, y, x, obj_block):
+        for cells in obj_block.values():
+            shape = []
+            fail = False        
+            for cell in cells:
+                xoffset = cell[1]
+                yoffset = cell[0]
+                try:
+                    test = self.board[y+yoffset][x+xoffset]
+                    shape.append((y+yoffset,x+xoffset))
+                except:
+                    shape = []
+                    fail = True
+            if not fail:
+                for add in shape:
+                    self.board[add[0]][add[1]] += 1
 
-    def place_block(self, x, y, obj_block):
-        pass
+    def place_block(self, y, x, obj_block, angle):
+        self.board = 
     
     def display_board(self):
         display = ""
         for row in self.board:
             display += "|"
             for col in row:
-                display += str(col)+"|"
+                display += str(col).zfill(3)+"|"
             display += "\n"
         print(display)
 
