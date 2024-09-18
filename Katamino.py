87c5f9fad5b05256ed7b0a86844ccd9fc1f6567d
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2024-09-18 11:15:36 +0300

    Update Katamino.py

--- Katamino.py
+++ Katamino.py
@@ -1,106 +1,125 @@
 from Blocks import Blocks
 
+Ghost = "Ghost"
+Solid = "Solid"
+Dead = "Daed"
+
+class Obj_Block:
+    def __init__(self):
+        self.color = "" # Blue, Orange, Grey string
+        self.ghosts = [] # [ghost1, ghost2, ghost3] a list of ghost objects
+        self.selected_ghost = 0 # the index of the solid ghost int
+
+class Obj_Ghost:
+    def __init__(self, ghost_name, shape):
+        self.name = ghost_name # ghost1, ghost2, ghost3 string
+        self.cells = shape # [(0,0),(1,0),(2,0),(3,0),(4,0)] a list of locations on the board
+        self.state = Ghost # Ghost, Solid, Dead string
+
 class main:
     def __init__(self):
-        self.blocks = Blocks()
-        self.board = []
-        self.playing_blocks = []
-        self.ghosts = {}
-        self.ghosts_status = {}
-        self.contact_points = {}
+        self.blocks = Blocks() # load the shapes of all the blocks
+        self.objects = {} # a dict of block objects with name for selecting
+        self.ghosts = {} # a list of ghosts objects for refernce (for collision code)
+        self.board = [] # 2d list where each col is list of ghost names that are located there
         self.ghost_index = 0
 
-        ################  for testing  ################
-        self.set_board_size(3)
+        #### TEST ####
+        self.setup_board(3)
+        self.create_object(self.blocks.Orange)
+        self.create_object(self.blocks.Brown)
+        self.create_object(self.blocks.Green)
+        self.make_ghost_solid(self.objects["Orange"].ghosts[0])
         self.display_board()
-        self.add_object_to_game(self.blocks.Orange)
-        #self.add_object_to_game(self.blocks.Brown)
-        #self.add_object_to_game(self.blocks.Green)
-        self.display_board()
-        self.place_block(0,0,self.playing_blocks[0],3)
-        self.display_board()
-        ###############################################
+        ##############
+
+    def calculate(self):
+        # the calculation method:
+        # START LOOP
+        # go to next object in self.objects (start from 0)
+        # set the objects' selected_ghost proparty to the next index (start from 0)
+        # go to the next object in the list self.objects
+        # select the first available ghost and set selected_ghost to the index of the ghost
+        # if there are no available shosts at go back to the prev self.objects index and goto START LOOP
+        pass
     
-    def set_board_size(self, size):
+    def make_ghost_solid(self, obj_ghost):
+        if obj_ghost.state != Dead:
+            this_id = obj_ghost.name
+            obj_ghost.state = Solid
+            print(obj_ghost.cells)
+            for cell in obj_ghost.cells:
+                r = cell[0]
+                c = cell[1]
+                selected_board_cell = self.board[r][c] # print(selected_board_cell) > [1, 3, 4, 17, 20, 33, 36]
+                for ghost_id in selected_board_cell: # kill all ghosts coliding with the solid ghost
+                    if ghost_id != this_id: # dont kill yourself
+                        self.ghosts[ghost_id].state = Dead # kill ghost on collision
+    
+    def create_object(self, obj_block):
+        # run a for loop throgh the board
+        r = 0
+        c = 0
+        block = Obj_Block()
+        for row in self.board: # row = [[],[],[]]
+            for col in row: # col = []
+                # now place ghosts of the object in all its angles
+                name = ""
+                for angle, cells in obj_block.items(): # angle: Name, 0, 1, 2, 3, 4, cells: "Orange", ((0,0),(0,1),(1,0),(2,0),(3,0))
+                    if angle != "Name":
+                        fail = False
+                        shape = []
+                        # go over each cell ((0,0),(0,1),(1,0),(2,0),(3,0))
+                        for cell in cells:
+                            roffset = r+cell[0]
+                            coffset = c+cell[1]
+
+                            # try to see if this offset is within the borders of the board
+                            try:
+                                test = self.board[roffset][coffset]
+                                shape.append((roffset,coffset))
+                            except:
+                                fail = True
+                                shape = []
+                        
+                        # after the set ((0,0),(0,1),(1,0),(2,0),(3,0)) is placed on the board, if it succeeded 
+                        if not fail:
+                            # the shape if a set of:
+                            # print("r=",r,"c=",c,"angle=",angle,"shape=",shape)
+                            # r= 0 c= 0 angle= 0 shape= [(0, 0), (0, 1), (1, 0), (2, 0), (3, 0)]
+                            # create a ghost nd insert it into the block object
+                            self.ghost_index += 1
+                            ghost = Obj_Ghost(self.ghost_index, shape)
+                            block.ghosts.append(ghost)
+                            self.ghosts[self.ghost_index] = ghost
+                            for cell in shape:
+                                self.board[cell[0]][cell[1]].append(self.ghost_index)
+                    else:
+                        name = cells
+                        block.color = name
+                        self.objects[name] = block
+                # add cell
+                c += 1
+            # add row
+            c = 0
+            r += 1
+
+
+    def setup_board(self, size):
         for row in range(5):
             self.board.append([])
             for _ in range(size):
-                self.board[row].append(0)
-
-    def add_object_to_game(self, obj_block):
-        self.playing_blocks.append(obj_block)
-        y = 0
-        x = 0
-        for row in self.board:
-            for _ in row:
-                self.create_ghost(y,x,obj_block)
-                x += 1
-            x = 0
-            y += 1
-
-    def create_ghost(self, y, x, obj_block):
-        index = self.ghost_index
-        self.ghost_index += 1
-        for key, cells in obj_block.items():
-            if key != "Name":
-                shape = []
-                fail = False        
-                for cell in cells:
-                    xoffset = cell[1]
-                    yoffset = cell[0]
-                    try:
-                        test = self.board[y+yoffset][x+xoffset]
-                        shape.append((y+yoffset,x+xoffset))
-                    except:
-                        shape = []
-                        fail = True
-                if not fail:
-                    for add in shape:
-                        yy = add[0]
-                        xx = add[1]
-                        self.board[yy][xx] += 1
-                        if not index in self.ghosts:
-                            self.ghosts[index] = []
-                        self.ghosts[index].append((yy,xx))
-                        self.ghosts_status[index] = True
-                        loc = str(yy)+":"+str(xx)
-                        if not loc in self.contact_points:
-                            self.contact_points[loc] = []
-                        self.contact_points[loc].append(index)
-
-    def place_block(self, y, x, obj_block, angle):
-        for cell in obj_block[angle]:
-            yoffset = y+cell[0]
-            xoffset = x+cell[1]
-            self.delete_ghost(yoffset, xoffset)
-
-    def delete_ghost(self, y, x):
-        loc = str(y)+":"+str(x)
-        ghosts = self.contact_points[loc]
-
-        for index in ghosts:
-            if self.ghosts_status[index]:
-                for cell in self.ghosts[index]:
-                    y = cell[0]
-                    x = cell[1]
-                    if self.board[y][x] > 0:
-                        self.board[y][x] -= 1
-                self.ghosts_status[index] = False
+                self.board[row].append([])
     
     def display_board(self):
         display = ""
         for row in self.board:
             display += "|"
             for col in row:
-                display += str(col).zfill(3)+"|"
+                for cell in col:
+                    display += "G"+str(cell)+" "+self.ghosts[cell].state+","
+                display += "|"
             display += "\n"
-        print("BOAD:")
         print(display)
-        print("ghosts:")
-        print(self.ghosts)
-        print("\ncontact_points:")
-        print(self.contact_points)
-        print("\nghosts_status:")
-        print(self.ghosts_status)
 
 main()
