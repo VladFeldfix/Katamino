80e07fb8026c0a7ba21f5ad2044eaceddbd4de03
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2024-10-29 00:13:54 +0200

    general sync

--- install.cmd
+++ install.cmd
@@ -1,1 +1,3 @@
-pyinstaller -i favicon.ico --onefile --noconsole Katamino.py
+set "currentDirectory=%cd%
+pyinstaller --distpath %currentDirectory% -i favicon.ico --onefile --noconsole Katamino.py
+pause
